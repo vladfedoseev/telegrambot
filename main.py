323b22ed3d836dff6148b3af89152016c918b257
vladfedoseev <59650684+vladfedoseev@users.noreply.github.com>
2020-04-20 21:31:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,71 @@
+import requests
+import datetime
+from time import sleep
+
+url = "https://api.telegram.org/bot1086475684:AAG79ro4fY3zXmUQxb1t23OgWOdDJRlHfOI/"
+
+
+def get_updates_json(request):
+    params = {'timeout': 100, 'offset': None}
+    response = requests.get(request + 'getUpdates', data=params)
+    return response.json()
+
+
+def last_update(data):
+    results = data['result']
+    total_updates = len(results) - 1
+    return results[total_updates]
+
+
+def get_chat_id(update):
+    chat_id = update['message']['chat']['id']
+    return chat_id
+
+
+def send_mess(chat, text):
+    params = {'chat_id': chat, 'text': text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+
+
+def main():
+    update_id = last_update(get_updates_json(url))['update_id']
+    while True:
+        if update_id == last_update(get_updates_json(url))['update_id']:
+            send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
+            update_id += 1
+    sleep(1)
+
+
+if __name__ == '__main__':
+    main()
+
+
+class BotHandler:
+
+    def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+
+    def get_updates(self, timeout=30):
+        method = 'getUpdates'
+        params = {'timeout': timeout}
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()['result']
+        return result_json
+
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
+
+    def get_last_update(self):
+        get_result = self.get_updates()
+
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+        else:
+            last_update = get_result[len(get_result)]
+
+        return last_update
